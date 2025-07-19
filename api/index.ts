14881ba13e19738199b7829cbce5eceeaab3e5f3
afraca130 <zokatn@gmail.com>
2025-07-19 18:02:54 +0900

    feat: vercel 배포 환경 변경

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { AppModule } from '../dist/app.module';
-import { ResponseInterceptor } from '../dist/common/interceptors/response.interceptor';
-import { GlobalExceptionFilter } from '../dist/common/filters/global-exception.filter';
-import { JwtAuthGuard } from '../dist/common/guards/jwt-auth.guard';
-import { RolesGuard } from '../dist/common/guards/roles.guard';
 import { Reflector } from '@nestjs/core';
-import { settingSwagger } from '../dist/common/utils/swagger/swagger.util';
+import { AppModule } from '../src/app.module';
+import { GlobalExceptionFilter } from '../src/common/filters/global-exception.filter';
+import { JwtAuthGuard } from '../src/common/guards/jwt-auth.guard';
+import { RolesGuard } from '../src/common/guards/roles.guard';
+import { ResponseInterceptor } from '../src/common/interceptors/response.interceptor';
+import { settingSwagger } from '../src/common/utils/swagger/swagger.util';
 
 let app: any;
 
