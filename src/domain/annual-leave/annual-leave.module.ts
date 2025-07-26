2296019aacc439f8848168aaafaa93197899ec81
afraca130 <zokatn@gmail.com>
2025-07-26 20:42:46 +0900

    FEAT: 연차 일정 기록 도메인 모듈 구현

--- src/domain/annual-leave/annual-leave.module.ts
+++ src/domain/annual-leave/annual-leave.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeeAnnualLeaveEntity } from './entities/employee-annual-leave.entity';
+import { AnnualLeaveDomainService } from './services/annual-leave-domain.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([EmployeeAnnualLeaveEntity])],
-  providers: [],
-  controllers: [],
-  exports: [TypeOrmModule],
+    imports: [TypeOrmModule.forFeature([EmployeeAnnualLeaveEntity])],
+    providers: [AnnualLeaveDomainService],
+    exports: [AnnualLeaveDomainService],
 })
 export class AnnualLeaveModule {}
